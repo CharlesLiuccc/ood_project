0b90983951bd6bf4949d1323e055a512759853ee
CharlesLiuccc <charlesliucccc@gmail.com>
2022-04-03 18:43:26 -0400

    update jdbc dependencies

---
+++ backend/src/main/java/com/gwu/backend/Controller/test.java
@@ -0,0 +1,12 @@
+package com.gwu.backend.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class test {
+    @RequestMapping("/index")
+    public String Hello(){
+        return "index";
+    }
+}
