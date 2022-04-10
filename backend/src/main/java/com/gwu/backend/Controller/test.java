ece7445b5e2dc8196646ed64c74766e5fe85c396
liuhq7 <44065019+liuhq7@users.noreply.github.com>
2022-04-10 17:41:15 -0400

    Update test.java

--- backend/src/main/java/com/gwu/backend/Controller/test.java
+++ backend/src/main/java/com/gwu/backend/Controller/test.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class test {
     @RequestMapping("/index")
-    public String Hello(){
+    public String index(){
         return "index";
     }
     @RequestMapping("main")
@@ -18,7 +18,43 @@ public class test {
         return "login";
     }
     @RequestMapping("SymptomInfo")
-    public String sympotomInfo(){
+    public String SymptomInfo(){
         return "SymptomInfo";
+    }
+    @RequestMapping("DoctorVisitInfo")
+    public String DoctorVisitInfo(){
+        return "DoctorVisitInfo";
+    }
+    @RequestMapping("form")
+    public String form(){
+        return "form";
+    }
+    @RequestMapping("MedicineInfo")
+    public String MedicineInfo(){
+        return "MedicineInfo";
+    }
+    @RequestMapping("Register")
+    public String Register(){
+        return "Register";
+    }
+    @RequestMapping("RelatedNews")
+    public String RelatedNews(){
+        return "RelatedNews";
+    }
+    @RequestMapping("TakeOutsInfo")
+    public String TakeOutsInfo(){
+        return "TakeOutsInfo";
+    }
+    @RequestMapping("table")
+    public String table(){
+        return "table";
+    }
+    @RequestMapping("TripsInfo")
+    public String TripsInfo(){
+        return "TripsInfo";
+    }
+    @RequestMapping("welcome")
+    public String welcome(){
+        return "welcome";
     }
 }
